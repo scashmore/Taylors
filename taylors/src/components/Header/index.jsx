c9172189a16b6572819f3740691270ad0a14c8bd
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-30 18:37:39 -0400

    navigation bar layout

---
+++ taylors/src/components/Header/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Navbar, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap'
+import logo from '../../images/logo.png'
+
+function Header() {
+    return (
+        <>
+            <Navbar bg="light" expand="lg" sticky="top">
+            <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="220vw"
+                        className="d-inline-block align-top"
+                        alt="OPT logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Item>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#aboutus">About</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#brevard">Brevard</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#asheville">Asheville</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#greenville">Greenville</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header
