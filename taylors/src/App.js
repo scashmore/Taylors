8ff05918cbfa7ef319989aade3d52d644d413e85
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-30 23:10:55 -0400

    working on homepage

--- taylors/src/App.js
+++ taylors/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+  <>
+    <div className="App bg"></div>
       <Header />
-    </div>
+      <Home/>
+    </>
   );
 }
 
---
+++ taylors/src/pages/Home/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import './style.css'
+
+function Home() {
+    return (
+        <>
+            <div>
+                <Card>
+                    <Card.Img className="hero" src='https://picsum.photos/200/300?grayscale' />
+                </Card>
+                <Card className="home">
+                    <p>The original store, located in beautiful downtown Brevard, North Carolina, 
+                        is over 6,000 square feet of fun and hands-on excitement. There is a reason 
+                        that O.P. Taylor’s continues to be the #1 toy store in the south, so come and experience 
+                        it for yourself!​ All of O.P. Taylor’s stores are strategically located near 
+                        terrific restaurants and other fascinating stores. Either make a day of it 
+                        and visit one of our stores, or, get up early and visit all three. We can’t 
+                        wait to see you!</p>
+                        <img className="img" src='https://picsum.photos/400/300?grayscale'/>
+                </Card>
+                <Card className='home'>
+                    <p>
+                        Be sure to checkout our online store on Amazon! {' '}
+                        <a href="https://www.amazon.com/s?me=A1LTJY4HYKZU11&marketplaceID=ATVPDKIKX0DER&redirect=true" target="_blank">
+                            Click here to shop online now!</a>
+                    </p>
+                </Card>
+            </div>
+        </>
+    )
+
+}
+
+export default Home
